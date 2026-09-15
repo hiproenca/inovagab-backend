85bf888d47dc8da6fc58c5c4d6705cd2c0fb4572
higor.proenca <higorproenca@gmail.com>
2026-09-15 15:04:44 -0300

    feat: implementa configurações globais do Spring Security
    Integra o JwtAuthenticationFilter, configura rotas públicas, define sessão stateless e ajusta a injeção de dependência do DaoAuthenticationProvider utilizando Lambda DSL do Spring Boot 3.

---
+++ src/main/java/com/hig/inovagab/security/SpringSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.hig.inovagab.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+    private static final String API_AUTH_URL_PREFIX = "/api/auth/**";
+    public static final String ACTUATOR_ENDPOINTS_URL_PREFIX = "/actuator/**";
+
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtTokenFilter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(API_AUTH_URL_PREFIX).permitAll()
+                        .requestMatchers(ACTUATOR_ENDPOINTS_URL_PREFIX).permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
